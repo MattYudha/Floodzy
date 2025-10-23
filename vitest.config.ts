5d7bcb130b2a6c76e0fd97f875b3ccc2c80f010a
MattYudha <dewarahmat12334@gmail.com>
2025-10-23 10:41:47 +0700

    push finnal pnpm

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,11 +5,19 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  optimizeDeps: {
+    include: ['@testing-library/jest-dom'],
+  },
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './vitest.setup.ts',
     include: ['**/*.test.tsx', '**/*.test.ts'],
     exclude: ['node_modules', 'dist', '.next', 'out'],
+    coverage: {
+      provider: 'v8', // Explicitly set the provider
+      reporter: ['text', 'json', 'html'], // Example reporters
+      exclude: ['node_modules/', '.next/', 'out/', 'vitest.config.ts', '**/*.d.ts'], // Exclude common files
+    },
   },
 });
