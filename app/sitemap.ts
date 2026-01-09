278895d992698d2d894eda72da320a9ac2d54b55
MattYudha <dewarahmat12334@gmail.com>
2026-01-09 17:50:49 +0700

    push sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,40 @@
+export default function sitemap() {
+    return [
+        {
+            url: 'https://floodzy.id',
+            lastModified: new Date().toISOString(),
+        },
+        {
+            url: 'https://floodzy.id/monitoring-banjir',
+            lastModified: new Date().toISOString(),
+        },
+        {
+            url: 'https://floodzy.id/prakiraan-cuaca',
+            lastModified: new Date().toISOString(),
+        },
+        {
+            url: 'https://floodzy.id/peringatan',
+            lastModified: new Date().toISOString(),
+        },
+        {
+            url: 'https://floodzy.id/lapor-banjir',
+            lastModified: new Date().toISOString(),
+        },
+        {
+            url: 'https://floodzy.id/info-evakuasi',
+            lastModified: new Date().toISOString(),
+        },
+        {
+            url: 'https://floodzy.id/data-sensor',
+            lastModified: new Date().toISOString(),
+        },
+        {
+            url: 'https://floodzy.id/statistik-data',
+            lastModified: new Date().toISOString(),
+        },
+        {
+            url: 'https://floodzy.id/tentang-floodzy',
+            lastModified: new Date().toISOString(),
+        },
+    ];
+}
